4c9c57d756d6dca86b6c0b5e19c0c277dee86fcc
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-25 11:41:37 +0700

    Laravel Multi-Auth

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,12 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
+    public static function redirectTo($guard)
+    {
+        return $guard . '/dashboard';
+    }
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AdminProfileController;
+use App\Http\Controllers\Backend\ProfileVillageController;
+use App\Http\Controllers\Backend\ProgramCategoryController;
+use App\Http\Controllers\Backend\ProgramController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +21,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// admin:admin -> adalah merupakan middleware
+Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function () {
+    Route::get('/login', [AdminController::class, 'loginForm']);
+    Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
+});
+
+
+// kata admin setelah sacntum adalah nama guard
+Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+    return view('dashboard');
+})->name('admin.dashboard')->middleware('auth:admin');
+
+// kata web setelah sacntum adalah nama guard
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
