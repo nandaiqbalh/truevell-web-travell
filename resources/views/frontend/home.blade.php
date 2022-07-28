10700a84753a3f67d449d43dba2d5cbb3244d053
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-28 12:08:40 +0700

    Add Home Frontend

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,221 @@
+@extends('layouts.app')
+
+@section('title')
+Truevell
+@endsection
+
+@section('content')
+    <!-- Header -->
+    <header class="text-center">
+
+        <h1>
+            Travel, enjoy and live a new
+            <br>
+            and full life
+        </h1>
+
+        <p class="mt-3">
+            Built Wicket longer admire do barton vanity itself do in it.
+            <br>
+            Preferred to sportsmen it engrossed listening.
+            <br>
+            Park gate sell they west hard for the.
+        </p>
+
+        <a href="#" class="btn btn-get-started px-4 mt-4">Get Started</a>
+    </header>
+
+    <main>
+        <div class="container">
+            <section class="section-stats row justify-content-center" id="stats">
+                <div class="div col-3 col-md-2 stats-detail">
+                    <h2>20K</h2>
+                    <p>Members</p>
+                </div>
+
+                <div class="div col-3 col-md-2 stats-detail">
+                    <h2>25</h2>
+                    <p>Countries</p>
+                </div>
+
+                <div class="div col-3 col-md-2 stats-detail">
+                    <h2>5K</h2>
+                    <p>Hotels</p>
+                </div>
+
+                <div class="div col-3 col-md-2 stats-detail">
+                    <h2>120</h2>
+                    <p>Partners</p>
+                </div>
+            </section>
+        </div>
+
+        <section class="section-popular" id="popular">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center section-popular-heading">
+                        <h2>
+                            Popular Destinations
+                        </h2>
+
+                        <p>
+                            Visit our top testinations
+                            <br>
+                            and feel something you never feel before!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-popular-content" id="pupular-content">
+            <div class="container">
+                <div class="section-popular-travel row justify-text-center">
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('/frontend/images/travel-1.jpg') ;">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">DERATAN - BALI</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('/frontend/images/travel-2.jpg') ;">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">BROMO</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('/frontend/images/travel-3.jpg') ;">
+                            <div class="travel-country">INDONESIA</div>
+                            <div class="travel-location">NUSA PENIDA</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-travel text-center d-flex flex-column"
+                            style="background-image: url('/frontend/images/travel-4.jpg') ;">
+                            <div class="travel-country">MIDDLE EAST</div>
+                            <div class="travel-location">DUBAI</div>
+                            <div class="travel-button mt-auto">
+                                <a href="#" class="btn btn-travel-details px-4">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-networks">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4">
+                        <h2>Our Networks</h2>
+
+                        <p>
+                            Companies are trusted us
+                            <br>
+                            more than just a trip.
+                        </p>
+                    </div>
+
+                    <div class="col-md-8 text-center">
+                        <img src="/frontend/images/partner.png" alt="Logo Partners" class="img-parter">
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="section-testimonial-heading" id="testimonialHeading">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2>
+                            Customer Stories
+                        </h2>
+
+                        <p>
+                            Customer satisfaction is our priority.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="section section-testimonial-content" id="testimonialContent">
+            <div class="container">
+                <div class="section-popular-travel row justify-content-center">
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="/frontend/images/avatar-1.jpg" alt="User" class="rounded-circle">
+
+                                <h3 class="mb-4 mt-3">Jane Curty</h3>
+                                <p class="testimonial">
+                                    "It feels really good to be able to travel with Truevell services."
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">Trip to Nusa Penida</p>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="/frontend/images/avatar-2.jpg" alt="User" class="rounded-circle">
+
+                                <h3 class="mb-4 mt-3">Rapinha</h3>
+                                <p class="testimonial">
+                                    "It is a long established fact that a reader will be distracted by the readable
+                                    content of a page when looking at its layout."
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">Trip to Bromo</p>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <img src="/frontend/images/avatar-3.jpg" alt="User" class="rounded-circle">
+
+                                <h3 class="mb-4 mt-3">Margot Robbie</h3>
+                                <p class="testimonial">
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                </p>
+                            </div>
+                            <hr>
+                            <p class="trip-to mt-2">Trip to Nusa Penida</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="col-12 text-center">
+                        <a href="#" class="btn btn-need-help px-4 mt-4 mx-1">I Need Help</a>
+                        <a href="#" class="btn btn-get-started px-4 mt-4 mx-1">Get Started</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,148 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="frontend/libraries/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="frontend/styles/main.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Volkhov&display=swap" rel="stylesheet">
+</head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<body>
+    <!-- Semantic elements -->
+    <!-- https://www.w3schools.com/html/html5_semantic_elements.asp -->
 
-        <!-- Styles -->
-        @livewireStyles
+    <!-- Bootstrap navbar example -->
+    <!-- https://www.w3schools.com/bootstrap4/bootstrap_navbar.asp -->
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    <!-- Navbar -->
+    <div class="container">
+        <nav class="row navbar navbar-expand-lg navbar-light"
+            style="border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;">
+            <a class="navbar-brand" href="#">
+                <img src="frontend/images/logo.png" alt="" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navb">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            <!-- Menu -->
+            <div class="collapse navbar-collapse" id="navb">
+                <ul class="navbar-nav ml-auto mr-3">
+                    <li class="nav-item mx-md-2">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-md-2">
+                        <a class="nav-link" href="#">Paket Travel</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            Services
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Link 1</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item mx-md-2">
+                        <a class="nav-link" href="#">Testimonial</a>
+                    </li>
+                </ul>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                <!-- Mobile button -->
+                <form class="form-inline d-sm-block d-md-none">
+                    <button class="btn btn-login">
+                        Sign In
+                    </button>
+                </form>
+                <!-- Desktop Button -->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    <button class="btn btn-login">
+                        Sign In
+                    </button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    @yield('content')
+
+    <footer class="section-footer mt-5 mb-4 border-top">
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-12 col-lg-3">
+                                    <h5>FEATURES</h5>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">Reviews</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Community</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Social Media Kit</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Affiliate</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>ACCOUNT</h5>
+                                    <ul class="list-unstyled">
+                                        <li><a href="#">Refund</a></li>
+                                        <li><a href="#">Security</a></li>
+                                        <li><a href="#">Rewards</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>COMPANY</h5>
+                                    <ul class="list-unstyled">
+                                        <li><a href="#">Career</a></li>
+                                        <li><a href="#">Help Center</a></li>
+                                        <li><a href="#">Media</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <h5>Get Connected</h5>
+                                    <ul class="list-unstyled">
+                                        <li>Kulon Progo</li>
+                                        <li>Indonesia</li>
+                                        <li>0831 - 0148 - 7741</li>
+                                        <li>support@truevell.id</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                </div>
+            </div>
         </div>
+        <div class="container-fluid">
+            <div class="row border-top justify-content-center align-items-center pt-4">
+                <div class="col-auto text-gray-500 font-weight-light">
+                    2022 Copyright Truevell • All rights reserved • Made in Kulon Progo
+                </div>
+            </div>
+        </div>
+    </footer>
 
-        @stack('modals')
+    <main></main>
+    <footer></footer>
+    <script src="frontend/libraries/retina/retina.js"></script>
+    <script src="frontend/libraries/jquery/jquery-3.4.1.min.js"></script>
+    <script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
+</body>
 
-        @livewireScripts
-    </body>
 </html>
